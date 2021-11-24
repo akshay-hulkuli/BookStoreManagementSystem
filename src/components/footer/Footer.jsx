e478af193b7882b1c1dedd3ee503774af9d54999
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-24 17:57:38 +0530

    [Akshay]ADDED: footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './footer.scss'
+
+export default function Footer() {
+    return (
+        <div className="footer-style">
+            Copyright &copy; 2020, Bookstore Private Limited. All rights reserved.
+        </div>
+    )
+}
