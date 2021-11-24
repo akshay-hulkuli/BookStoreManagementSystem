63b5a38d619b09e9ec3fbe06d137ecf3295547a4
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-24 17:59:33 +0530

    [Akshay]ADDED: header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import icon from '../../assets/education.svg'
+import './header.scss'
+import { OutlinedInput, InputAdornment,IconButton, Box,Divider } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
+export default function Header() {
+    return (
+        <div class="root">
+            <div className="logo">
+                <img src={icon} style={{paddingTop:'10px'}}/>
+                <div class="header-text">bookstore</div>
+            </div>
+            <OutlinedInput
+                placeholder="Search"
+                name="search"
+                startAdornment={<InputAdornment position="start"><IconButton sx={{color:'rgb(26, 26, 26)'}}><SearchIcon/></IconButton></InputAdornment>}
+                sx={{width:{md:'40%',sm:'100%'},backgroundColor:'rgb(239, 239, 239)', color:'rgb(26, 26, 26)', margin:'5px 10px', borderRadius:'8px', height:'40px' }}  
+            />
+            <Box sx={{flexGrow:1}}/>
+            <div className="icons">
+                <Divider orientation="vertical" flexItem />
+                <div className="iconbox">
+                    <IconButton sx={{color:'white'}}><PersonOutlineOutlinedIcon/></IconButton>
+                    <div className="caption">account</div>
+                </div>
+                <Divider orientation="vertical" flexItem />
+                <div className="iconbox">
+                    <IconButton sx={{color:'white'}}><ShoppingCartOutlinedIcon/></IconButton>
+                    <div className="caption">cart</div>
+                </div>
+                <Divider orientation="vertical" flexItem />
+            </div>
+        </div>
+    )
+}
