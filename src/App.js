7f7a2fdb5656dd355b07ee38d69cac4d6cab7b9b
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-24 17:45:50 +0530

    [Akshay]ADDED: routes in the app.js component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 // import './App.css';
 import OrderLogin from './pages/landingPage/OrderLogin';
+import {Routes,BrowserRouter as Router,Route} from 'react-router-dom';
+import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
   return (
-    <div>
-      <OrderLogin/>
+    <div> 
+      <Router>
+        <Routes>
+          <Route exact path="/"  element={<OrderLogin/>}/>
+          <Route exact path="/dashboard" element={<Dashboard/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
