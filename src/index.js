b15e4f0359585c1dec56b813190c7a13745a258f
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-25 18:34:22 +0530

    [Akshay]ADDED: redux store in the index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore,applyMiddleware, compose} from 'redux';
+import { cartReducer } from './reducers';
+import { Provider } from 'react-redux';
+import BookService from './services/BookServices';
+import thunk from "redux-thunk"
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+
+const bookService = new BookService();
+
+let initialState = [];
+
+// bookService.getCartItems('bookstore_user/get_cart_items')
+//   .then((res)=>{
+//     console.log(res);
+//     initialState = res.data.result;
+//   })
+//   .catch((err)=>{
+//     console.log(err);
+//   })
+
+const myStore = createStore(
+  cartReducer, initialState, enhancer
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={myStore}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
