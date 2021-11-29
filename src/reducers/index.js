7e63de0aec4b07e849d24ae402ef033a5e3d3365
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-29 11:58:53 +0530

    [Akshay]REFACTORED: remove from cart case code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ export const cartReducer = (state = [], action) => {
     switch(action.type){
         case "ADDTOCART" : state.push(action.payload);
                            return state;
-        case "REMOVEFROMCART" : state = state.filter((data) => data.id != action.payload.id);
+        case "REMOVEFROMCART" : var index = state.indexOf(action.payload)
+                                state.splice(index,1)
                                 return state;
         case "INITIALISECART" : state = action.payload;
                                 return state;
