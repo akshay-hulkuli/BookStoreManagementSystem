49ae84a3ad0aaaa81829d18d137030b8171e1e14
Akshaya HP <akshayhulkuli@gmail.com>
2021-12-04 11:19:29 +0530

    [Akshay]ADDED: preloader to the dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -15,6 +15,7 @@ import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
 import {useDispatch,useSelector} from 'react-redux';
 import { addToCart,removeFromCart, initialiseCart, addToWishList, initializeWishList } from '../../actions';
+import bookloader from '../../assets/bookLoad.gif'
 
 const theme = createTheme({
   palette: {
@@ -64,6 +65,7 @@ export default function Dashboard() {
     const [backup,setBackup] = React.useState([]);
     const [currentPage, setCurrentPage] = React.useState(1);
     const [wishlist, setWishlist] = React.useState([]);
+    const [loading, setLoading] = React.useState(true);
     const booksPerPage = 8;
     const dispatch = useDispatch();
     const cartData = useSelector(state => state);
@@ -112,6 +114,9 @@ export default function Dashboard() {
     React.useEffect(()=>{
         getCart();
         getWishList();
+        setTimeout(()=>{
+            setLoading(false);
+        },2000);
     },[])
 
     React.useEffect(()=>{
@@ -206,73 +211,76 @@ export default function Dashboard() {
     }
 
     return (
-        <div>
-            <Header mode="dashboard" bookData={bookData} setBookData ={setBookData} backup={backup}/>
-            <div className="main">
-                <div className="main-header">
-                    <div className="main-header-left"><span style={{fontSize:'24px'}}>Books</span><span style={{color:'#9D9D9D'}} id="items">({bookData.length} items)</span></div>
-                    <div>
-                        <Box sx={{ minWidth: {md:200}}}>
-                            <FormControl fullWidth>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    value={sortingFilter}
-                                    onChange={handleChange}
-                                    IconComponent={KeyboardArrowDownOutlinedIcon}
-                                    size='small'
-                                    renderValue = {()=> {
-                                        if(sortingFilter === -1 )
-                                            return <em>select sorting option</em>
-                                        else
-                                            return <em>{filterOptions[sortingFilter-1]}</em>
-                                    }}
-                                >
-                                    <MenuItem value={1}>Price: Low to High</MenuItem>
-                                    <MenuItem value={2}>Price: High to Low</MenuItem>
-                                    <MenuItem value={3}>Title: A-Z</MenuItem>
-                                    <MenuItem value={4}>Title: Z-A</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Box>
+        <React.Fragment>
+            {loading ? <div className="preloader"><img src={bookloader}/></div> : 
+            <div>
+                <Header mode="dashboard" bookData={bookData} setBookData ={setBookData} backup={backup}/>
+                <div className="main">
+                    <div className="main-header">
+                        <div className="main-header-left"><span style={{fontSize:'24px'}}>Books</span><span style={{color:'#9D9D9D'}} id="items">({bookData.length} items)</span></div>
+                        <div>
+                            <Box sx={{ minWidth: {md:200}}}>
+                                <FormControl fullWidth>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={sortingFilter}
+                                        onChange={handleChange}
+                                        IconComponent={KeyboardArrowDownOutlinedIcon}
+                                        size='small'
+                                        renderValue = {()=> {
+                                            if(sortingFilter === -1 )
+                                                return <em>select sorting option</em>
+                                            else
+                                                return <em>{filterOptions[sortingFilter-1]}</em>
+                                        }}
+                                    >
+                                        <MenuItem value={1}>Price: Low to High</MenuItem>
+                                        <MenuItem value={2}>Price: High to Low</MenuItem>
+                                        <MenuItem value={3}>Title: A-Z</MenuItem>
+                                        <MenuItem value={4}>Title: Z-A</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Box>
+                        </div>
                     </div>
+                    <Grid container spacing={4} sx={{minHeight:'68vh'}}>
+                        {currentBooks.map((book,index)=>(
+                            <Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
+                                <div className='card'>
+                                    <div className="card-media">
+                                        <img src={image} className="image"/>
+                                    </div>
+                                    <div className="book-details">
+                                        <div className="book-name">{book.bookName}</div>
+                                        <div className="book-author">by {book.author}</div>
+                                        <div className="book-price">Rs. {book.price}</div>
+                                    </div>
+                                    {buttons(book)}
+                                    {/* {console.log(cartData)} */}
+                                </div>
+                            </Grid>
+                        ))}
+                    </Grid>
+                    <ThemeProvider theme={theme}>
+                        <Stack direction='row' justifyContent="center" sx={{padding:'30px'}}>
+                            <Pagination 
+                                count={((bookData.length-(bookData.length%booksPerPage))/booksPerPage)+1} 
+                                onChange={handlePageChange} 
+                                shape="rounded"
+                                color="myColor"
+                                renderItem={(item) => (
+                                    <PaginationItem
+                                    components={{ previous: a, next: b }}
+                                    {...item}
+                                    />
+                                )}
+                            />
+                        </Stack>
+                    </ThemeProvider>
                 </div>
-                <Grid container spacing={4} sx={{minHeight:'68vh'}}>
-                    {currentBooks.map((book,index)=>(
-                        <Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
-                            <div className='card'>
-                                <div className="card-media">
-                                    <img src={image} className="image"/>
-                                </div>
-                                <div className="book-details">
-                                    <div className="book-name">{book.bookName}</div>
-                                    <div className="book-author">by {book.author}</div>
-                                    <div className="book-price">Rs. {book.price}</div>
-                                </div>
-                                {buttons(book)}
-                                {/* {console.log(cartData)} */}
-                            </div>
-                        </Grid>
-                    ))}
-                </Grid>
-                <ThemeProvider theme={theme}>
-                    <Stack direction='row' justifyContent="center" sx={{padding:'30px'}}>
-                        <Pagination 
-                            count={((bookData.length-(bookData.length%booksPerPage))/booksPerPage)+1} 
-                            onChange={handlePageChange} 
-                            shape="rounded"
-                            color="myColor"
-                            renderItem={(item) => (
-                                <PaginationItem
-                                  components={{ previous: a, next: b }}
-                                  {...item}
-                                />
-                              )}
-                        />
-                    </Stack>
-                </ThemeProvider>
-            </div>
-            <Footer/>
-        </div>
+                <Footer/>
+            </div>  }
+        </React.Fragment>
     )
 }
